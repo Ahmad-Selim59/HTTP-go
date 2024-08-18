2ce7b672069b341050882297de57fb48da242952
Ahmad-Selim59 <ahmad22selim@gmail.com>
2024-08-18 14:19:56 +0100

    making a manual HTTP request

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"rsc.io/quote/v4"
+	"net"
 )
 
 func main() {
-	fmt.Println(quote.Glass())
+	tcpServer("httpbin.org", 80)
+}
+
+func tcpServer(address string, portNum int) {
+	url := fmt.Sprintf("%s:%d", address, portNum)
+	conn, err := net.Dial("tcp", url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	fmt.Fprintf(conn, "GET /anything HTTP/1.0\r\n\r\n")
+	status, err := bufio.NewReader(conn).ReadString('\n')
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(status)
+	}
 }
